5beed525f0375890f20586528db5f9386afdeb1a
nozu003 <81842374+nozu003@users.noreply.github.com>
2021-04-07 15:29:53 +0800

    Quick update

--- js/films.js
+++ js/films.js
@@ -50,6 +50,7 @@ $(document).ready(function(){
 
             $(".header__section4--collection-display").addClass("disp-none");
             $(".selection-photo-lr").removeClass("disp-none");
+
         }
         
         /* films-collections */
@@ -102,6 +103,18 @@ $(document).ready(function(){
                 filmCollections();
             }
             else if($(".header__section4--film-present").hasClass("disp-none")){
+                if(document.querySelector('.film-title-player').innerHTML == "LARUAN"){
+                    document.getElementById('background').style.backgroundImage = 'url('+imageBackground[0]+')';
+                }
+                else if(document.querySelector('.film-title-player').innerHTML == "PROJECT: DAY OFF"){
+                    document.getElementById('background').style.backgroundImage = 'url('+imageBackground[1]+')';
+                }
+                else if(document.querySelector('.film-title-player').innerHTML == "LARAWAN"){
+                    document.getElementById('background').style.backgroundImage = 'url('+imageBackground[2]+')';
+                }
+                else if(document.querySelector('.film-title-player').innerHTML == "INTRA &mdash; SANTIAGO"){
+                    document.getElementById('background').style.backgroundImage = 'url('+imageBackground[3]+')';
+                }
                 filmPresent();
             }
             else{
@@ -128,6 +141,7 @@ $(document).ready(function(){
 
         document.querySelector('.header__section4--portfolio-nav--portfol').innerHTML = "PORTFOLIOS ";
         document.getElementById('background').style.backgroundImage = '';
+        
     }
 
     function homeClick(){
@@ -174,12 +188,12 @@ $(document).ready(function(){
       var film3 = $(".film--3");
       var film4 = $(".film--4");
 
-      var imageBackground = ['./webp/films/vlcsnap-2021-03-26-18h30m05s129-min.webp', './webp/films/vlcsnap-2021-03-26-18h30m05s128-min.webp', './webp/films/vlcsnap-2021-03-26-18h30m05s128sss-min.webp', './webp/films/vlcsnap-2021-03-26-18h30m05s130-min.webp']
+      var imageBackground = ['./webp/blurred/blurred-bg2-min.webp', './webp/blurred/blurred-bg4-min.webp', './webp/blurred/blurred-bg3-min.webp', './webp/blurred/blurred-bg1-min.webp']
 
       film1.click(function(){
         document.getElementById("videoPlayer").src = "https://www.youtube-nocookie.com/embed/DRrg1gC0fhM?modestbranding=1&showinfo=0&rel=0&iv_load_policy=3&theme=light&color=white&disablekb=1";
 
-        document.getElementById('background').style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.750), rgba(0, 0, 0, 0.750)),url('+imageBackground[0]+')';
+        document.getElementById('background').style.backgroundImage = 'url('+imageBackground[0]+')';
 
         document.querySelector('.film-title-player').innerHTML = "LARUAN";
 
@@ -189,7 +203,7 @@ $(document).ready(function(){
       film2.click(function(){
         document.getElementById("videoPlayer").src = "https://www.youtube-nocookie.com/embed/CoBoAi_tAdA?modestbranding=1&showinfo=0&rel=0&iv_load_policy=3&theme=light&color=white&disablekb=1";
 
-        document.getElementById('background').style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.750), rgba(0, 0, 0, 0.750)),url('+imageBackground[1]+')';
+        document.getElementById('background').style.backgroundImage = 'url('+imageBackground[1]+')';
 
         document.querySelector('.film-title-player').innerHTML = "PROJECT: DAY OFF";
 
@@ -199,7 +213,7 @@ $(document).ready(function(){
       film3.click(function(){
         document.getElementById("videoPlayer").src = "https://www.youtube-nocookie.com/embed/mbJbR_AV0Fc?modestbranding=1&showinfo=0&rel=0&iv_load_policy=3&theme=light&color=white&disablekb=1";
         
-        document.getElementById('background').style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.750), rgba(0, 0, 0, 0.750)),url('+imageBackground[2]+')';
+        document.getElementById('background').style.backgroundImage = 'url('+imageBackground[2]+')';
 
         document.querySelector('.film-title-player').innerHTML = "LARAWAN";
 
@@ -209,7 +223,7 @@ $(document).ready(function(){
       film4.click(function(){
         document.getElementById("videoPlayer").src = "https://www.youtube-nocookie.com/embed/zkbUvTHETds?modestbranding=1&showinfo=0&rel=0&iv_load_policy=3&theme=light&color=white&disablekb=1";
 
-        document.getElementById('background').style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.750), rgba(0, 0, 0, 0.750)),url('+imageBackground[3]+')';
+        document.getElementById('background').style.backgroundImage = 'url('+imageBackground[3]+')';
 
         document.querySelector('.film-title-player').innerHTML = "INTRA &mdash; SANTIAGO";
 
