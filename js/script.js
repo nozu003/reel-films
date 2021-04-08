12efb365ddd9ed85d7f2d4f15987e48a35971c23
nozu003 <81842374+nozu003@users.noreply.github.com>
2021-04-08 19:58:27 +0800

    Quick update

--- js/script.js
+++ js/script.js
@@ -11,6 +11,21 @@ $(document).ready(function(){
         } while (currentDate - date < milliseconds);
       }
 
+      $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html,body').animate({
+                scrollTop: target.offset().top
+              }, 1000);
+              return false;
+            }
+          }
+        });
+      });
+      
       H4AnimationRemover();
 
       function H4AnimationRemover(){
