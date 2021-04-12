09c7683c9a5367be161d8dd8fc12b800bda76817
nozu003 <81842374+nozu003@users.noreply.github.com>
2021-04-12 14:27:29 +0800

    Quick update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
     var mPanel = $(".main-panel");
+    var mPanelCont = $(".main-panel-container")
 
     if(mPanel.hasClass("init-scale")){
         setTimeout(function(){
@@ -8,6 +9,16 @@ $(document).ready(function(){
             mPanel.addClass("def-scale");
         }
         , 1000
+        );
+        setTimeout(function(){
+            $(".main-panel-navigation").addClass("animate__animated animate__fadeIn animate__slow");
+            $(".credentials").addClass("animate__animated animate__fadeIn animate__slow");
+            $(".main-panel-navigation").addClass("opac-1");
+            $(".credentials").addClass("opac-1");
+            $(".main-panel-navigation").removeClass("opac-0");
+            $(".credentials").removeClass("opac-0");
+        }
+        , 500
         );
     }
 });
