c525b09205c74e9889dfdb81c9c6c2c0b74a2983
nozu003 <81842374+nozu003@users.noreply.github.com>
2021-04-15 16:12:39 +0800

    Quick update

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
     var mPanelCont = $(".main-panel-container");
     var films = $(".js--films");
     var gallery = $(".js--gallery");
+    const btnLeft = $(".btn-left");
+    const btnRight = $(".btn-right");
 
     if(mPanel.hasClass("init-scale")){
         setTimeout(function(){
@@ -39,8 +41,40 @@ $(document).ready(function(){
     if(document.title == "Reel Films | Portfolio"){
         
         films.click(function(){
-            location.href = "https://nozu003.github.io/reel-films/films";
+            location.href = "/films.html";
         });
+    }
+
+    if(document.title == "Reel Films | Films"){
+
+        var filmList = ["webp/films/vlcsnap-2021-03-26-18h30m05s128-min.webp", "webp/films/vlcsnap-2021-03-26-18h30m05s128sss-min.webp", "webp/films//vlcsnap-2021-03-26-18h30m05s129-min.webp","webp/films/vlcsnap-2021-03-26-18h30m05s130-min.webp"];
+
+        var index = 0;
+
+        const img1 = document.getElementById("img-display-1");
+        const img2 = document.getElementById("img-display-2");
+        const img3 = document.getElementById("img-display-3");
+
+        btnLeft.click(function(){
+            
+            index = 3;
+
+            img1.src = filmList[index--];
+            img2.src = filmList[index--];
+            img3.src = filmList[index--];
+            
+            console.log(index);
+
+        });
+
+        btnRight.click(function(){
+            
+            img1.src = filmList[index++];
+            img2.src = filmList[index++];
+            img3.src = filmList[index++];
+
+        });
+
     }
     
 });
