1da36bc4d8c3539a43d95c3560023fedd5dcf2e6
nozu003 <81842374+nozu003@users.noreply.github.com>
2021-06-30 18:13:57 +0800

    Quick update

--- js/script.js
+++ js/script.js
@@ -7,6 +7,17 @@ $(document).ready(function(){
     const btnLeft = $(".btn-left");
     const btnRight = $(".btn-right");
 
+    const filmList = ["../webp/films/vlcsnap-2021-03-26-18h30m05s128-min.webp", "../webp/films/vlcsnap-2021-03-26-18h30m05s128sss-min.webp", "../webp/films//vlcsnap-2021-03-26-18h30m05s129-min.webp","../webp/films/vlcsnap-2021-03-26-18h30m05s130-min.webp"];
+
+    const filmBGBlur = ["../webp/blurred/blurred-bg1-min.webp", "../webp/blurred/blurred-bg2-min.webp", "../webp/blurred/blurred-bg3-min.webp", "../webp/blurred/blurred-bg4-min.webp"];
+
+    const filmTitle = document.querySelector(".film-title");
+    const filmBG = document.getElementById("background");
+
+    const img1 = $(".selection--1");
+    const img2 = $(".selection--2");
+    const img3 = $(".selection--3");
+
     if(mPanel.hasClass("init-scale")){
         setTimeout(function(){
             mPanel.removeClass("init-scale");
@@ -41,40 +52,120 @@ $(document).ready(function(){
     if(document.title == "Reel Films | Portfolio"){
         
         films.click(function(){
-            location.href = "/films.html";
+            location.href = "/films/intramuros.html";
+        });
+        gallery.click(function(){
+            location.href = "/gallery.html";
         });
     }
 
     if(document.title == "Reel Films | Films"){
 
-        var filmList = ["webp/films/vlcsnap-2021-03-26-18h30m05s128-min.webp", "webp/films/vlcsnap-2021-03-26-18h30m05s128sss-min.webp", "webp/films//vlcsnap-2021-03-26-18h30m05s129-min.webp","webp/films/vlcsnap-2021-03-26-18h30m05s130-min.webp"];
-
         var index = 0;
 
-        const img1 = document.getElementById("img-display-1");
-        const img2 = document.getElementById("img-display-2");
-        const img3 = document.getElementById("img-display-3");
-
         btnLeft.click(function(){
-            
-            index = 3;
-
-            img1.src = filmList[index--];
-            img2.src = filmList[index--];
-            img3.src = filmList[index--];
-            
-            console.log(index);
-
+            alert("This is a button.");
         });
 
         btnRight.click(function(){
-            
-            img1.src = filmList[index++];
-            img2.src = filmList[index++];
-            img3.src = filmList[index++];
-
+            alert("This is a button.");
         });
 
+        if(filmTitle.innerHTML == "LARUAN"){
+            filmBG.style.backgroundImage = 'url('+filmBGBlur[1]+')';
+
+            img1.click(function(){
+                //day-off
+                location.href = "/films/day-off.html";
+            });
+
+            img2.click(function(){
+                //larawan
+                location.href = "/films/larawan.html";
+            });
+            
+            img3.click(function(){
+                //intra
+                location.href = "/films/intramuros.html"; 
+            });
+        }
+        if(filmTitle.innerHTML == "PROJECT: DAY OFF"){
+            filmBG.style.backgroundImage = 'url('+filmBGBlur[3]+')';
+
+            img1.click(function(){
+                //intra
+                location.href = "/films/intramuros.html";
+            });
+
+            img2.click(function(){
+                //larawan
+                location.href = "/films/larawan.html";
+            });
+            
+            img3.click(function(){
+                //laruan
+                location.href = "/films/laruan.html"; 
+            });
+        }
+        if(filmTitle.innerHTML == "LARAWAN"){
+            filmBG.style.backgroundImage = 'url('+filmBGBlur[2]+')';
+
+            img1.click(function(){
+                //day-off
+                location.href = "/films/day-off.html";
+            });
+
+            img2.click(function(){
+                //intra
+                location.href = "/films/intramuros.html";
+            });
+            
+            img3.click(function(){
+                //laruan
+                location.href = "/films/laruan.html";
+            });
+        }
+        if(filmTitle.innerHTML == "INTRA — SANTIAGO"){
+            filmBG.style.backgroundImage = 'url('+filmBGBlur[0]+')';
+    
+            img1.click(function(){
+                //day-off
+                location.href = "/films/day-off.html";
+            });
+    
+            img2.click(function(){
+                //larawan
+                location.href = "/films/larawan.html";
+            });
+            
+            img3.click(function(){
+                //laruan
+                location.href = "/films/laruan.html";
+            });
+        }
+        
+
+    }
+
+    if(document.title == "Reel Films | Collections"){
+        const film1 = $(".film--1"); //laruan
+        const film2 = $(".film--2"); //day-off
+        const film3 = $(".film--3"); //larawan
+        const film4 = $(".film--4"); //intra
+
+        film1.click(function(){
+            location.href = "/films/laruan.html";
+        });
+        film2.click(function(){
+            location.href = "/films/day-off.html";
+        });
+        film3.click(function(){
+            location.href = "/films/larawan.html";
+        });
+        film4.click(function(){
+            location.href = "/films/intramuros.html";
+        });
     }
     
+
 });
